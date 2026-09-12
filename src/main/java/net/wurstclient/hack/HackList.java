1f2337cd99245c321f3d1b067fbb5313cbd1fad1
retegrity <arteemtsyhankov@gmail.com>
2026-09-12 11:00:12 +0000

    Update HackList.java

--- src/main/java/net/wurstclient/hack/HackList.java
+++ src/main/java/net/wurstclient/hack/HackList.java
@@ -174,6 +174,7 @@ public final class HackList implements UpdateListener
 	public final SpeedHackHack speedHackHack = new SpeedHackHack();
 	public final SpeedNukerHack speedNukerHack = new SpeedNukerHack();
 	public final SpiderHack spiderHack = new SpiderHack();
+	public final StaffMonitorHack staffMonitorHack = new StaffMonitorHack();
 	public final StepHack stepHack = new StepHack();
 	public final TemplateToolHack templateToolHack = new TemplateToolHack();
 	public final ThrowHack throwHack = new ThrowHack();
